1d4a7de35c2eb1cb5718fc1c0eb2190788c75fe3
Joseph Wilbur <jcwilbur@gmail.com>
2021-12-14 10:55:47 -0500

    more efficient 6

--- Puzzle06/6b.2.py
+++ Puzzle06/6b.2.py
@@ -1,11 +1,7 @@
 import fileHelper
 daysToSimulate = 256
+fishCycleLength = 7
 newFishCycle = 9
-
-def SimulateDayWithCombinedFish(fishesIn):
-    zeroes = fishesIn[0]
-    fishesOutput = [fishesIn[1],fishesIn[2],fishesIn[3],fishesIn[4],fishesIn[5],fishesIn[6],fishesIn[7]+zeroes,fishesIn[8],zeroes]
-    return fishesOutput
 
 def GroupFish(fishes):
     fishesOut = [0] * newFishCycle
@@ -19,6 +15,9 @@ def SumFish(fishes):
         total += fish
     return total
 
+
+if newFishCycle < fishCycleLength: raise Exception("New fish cycle can't be shorter than the fish cycle length")
+
 inputLines = fileHelper.readInputFileAsStringList("Puzzle06/input6.txt")
 
 fishes = inputLines[0].split(',')
@@ -26,8 +25,12 @@ fishes = inputLines[0].split(',')
 for i in range(len(fishes)):
     fishes[i] = int(fishes[i])
 
+fishGroups = GroupFish(fishes)
 fishes = GroupFish(fishes)
 
 for i in range(daysToSimulate):
-    fishes = SimulateDayWithCombinedFish(fishes)
-    print("Day Complete: ", i, " ", SumFish(fishes))
+    dayInCycle = i % newFishCycle
+    newFishLocation = (dayInCycle + (fishCycleLength)) % newFishCycle
+    fishGroups[newFishLocation] += fishGroups[dayInCycle]
+    
+    print("Day ", i+1, "\n",fishGroups," Sum: ",SumFish(fishGroups))
