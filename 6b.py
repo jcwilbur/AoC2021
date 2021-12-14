686cec536ba98a243878c21a5438e51dff53648c
Joseph Wilbur <jcwilbur@gmail.com>
2021-12-13 21:26:27 -0500

    solved

--- 6b.py
+++ 6b.py
@@ -1,37 +1,33 @@
 import fileHelper
-
-cycleLength = 7
-newFishCycle = 9
 daysToSimulate = 256
+newFishCycle = 9
 
-def calculateSpawn(fish, numDays):
-    OGFishCycles = int(numDays/cycleLength)
+def SimulateDayWithCombinedFish(fishesIn):
+    zeroes = fishesIn[0]
+    fishesOutput = [fishesIn[1],fishesIn[2],fishesIn[3],fishesIn[4],fishesIn[5],fishesIn[6],fishesIn[7]+zeroes,fishesIn[8],zeroes]
+    return fishesOutput
 
-    return OGFishCycles * len(fishes)
+def GroupFish(fishes):
+    fishesOut = [0] * newFishCycle
+    for fish in fishes:
+        fishesOut[fish] +=1
+    return fishesOut
 
-def calculateFives(numDays):
-    cycles = (numDays // cycleLength) + 1
-    calc
-    return cycles
-
-def calculateSixes(numDays):
-    cycles = (numDays // cycleLength) + 1
-    if numDays % cycleLength ==0: cycles -=1
-    return cycles
-
-
-    
+def SumFish(fishes):
+    total = 0
+    for fish in fishes:
+        total += fish
+    return total
 
 inputLines = fileHelper.readInputFileAsStringList("input6.txt")
-
 
 fishes = inputLines[0].split(',')
 
 for i in range(len(fishes)):
     fishes[i] = int(fishes[i])
 
+fishes = GroupFish(fishes)
 
-anticipatedFish = calculateFish(fishes, daysToSimulate)
-
-
-print(anticipatedFish)
+for i in range(daysToSimulate):
+    fishes = SimulateDayWithCombinedFish(fishes)
+    print("Day Complete: ", i, " ", SumFish(fishes))
